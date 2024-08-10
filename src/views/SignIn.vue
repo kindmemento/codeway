758d1cd02c8c10bac64d42ebc93649ab73ac4ee8
kindmemento <kindmemento@gmail.com>
2024-08-10 15:08:24 +0300

    implement basic sign in form without styling

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,7 +1,15 @@
 <template>
-	<div>
-		<h1>Sign In Page</h1>
-		<div>CONTENT PLACEHOLDER</div>
+	<div class="signin-container">
+		<div class="signin-box">
+			<img src="../assets/icon.png" alt="Codeway Logo" class="logo" />
+			<h2>Please sign in</h2>
+			<form @submit.prevent="signIn">
+				<input type="email" placeholder="E-mail address" v-model="password" required />
+				<input type="password" placeholder="Password" v-model="password" required />
+				<button type="submit">Sign in</button>
+			</form>
+			<p>Codeway © 2021</p>
+		</div>
 	</div>
 </template>
 
