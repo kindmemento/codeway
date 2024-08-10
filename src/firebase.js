8749ea2b431d2b043dffc00c6b6c36e9bd40bc0a
kindmemento <kindmemento@gmail.com>
2024-08-10 16:36:30 +0300

    add firebase initialization configs

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDstTLfIXpNqoZJqStXmb7YPPPMRHrwDv0",
+  authDomain: "cortex-config.firebaseapp.com",
+  projectId: "cortex-config",
+  storageBucket: "cortex-config.appspot.com",
+  messagingSenderId: "370348194036",
+  appId: "1:370348194036:web:a438b70709fcd6467cc6a1",
+  measurementId: "G-DRSCNC93E1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
