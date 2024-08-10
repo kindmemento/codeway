ec9b3653efb47e1aefcfe15c460ea9ccfa5663a9
kindmemento <kindmemento@gmail.com>
2024-08-10 14:54:40 +0300

    separate routes from router as independent array

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../views/Home.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/signin',
+    name: 'SignIn',
+		// Route Level code-splitting
+		// This will generate a separate chunk which will be lazy-loaded when the route is visited.
+    component: () => import('../views/SignIn.vue'),
+  },
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+	history: createWebHistory(import.meta.env.BASE_URL), // @TODO: Check
+	routes
+});
 
 export default router
