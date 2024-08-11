bb69b7e9969de4e915447a6b165ac0210aeb8aab
kindmemento <kindmemento@gmail.com>
2024-08-12 00:15:38 +0300

    add a firestore service and implement service function getParameters

---
+++ src/firestore.js
@@ -0,0 +1,13 @@
+import { collection, getDocs } from "firebase/firestore";
+import { db } from './firebase';
+
+const getParameters = async () => {
+	const querySnapshot = await getDocs(collection(db, 'parameters'))
+	const params = []
+	querySnapshot.forEach((doc) => {
+		params.push({ id: doc.id, ...doc.data() })
+	})
+	return params
+}
+
+export { getParameters }
