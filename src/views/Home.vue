082a1a848b270551626b581ae7df5bf16ae3cbef
kindmemento <kindmemento@gmail.com>
2024-08-12 00:27:35 +0300

    retrieve parameters from firestore and render dynamically

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,232 +11,188 @@
 		</nav>
 
 		<div class="body-container">
+			<div class="header">
+				<div class="column">Parameter Key</div>
+				<div class="column">Value</div>
+				<div class="column">Description</div>
+				<div class="column">Create Date</div>
+				<div class="column"></div>
+			</div>
 
-			<div class="header-container">
-				<div class="header">
-					<div class="column">Parameter Key</div>
-					<div class="column">Value</div>
-					<div class="column">Description</div>
-					<div class="column">Create Date</div>
-					<div class="column"></div>
-				</div>
-	
-				<div class="rows-container">
-	
-					<div class="row">
-						<div class="column">min_version</div>
-						<div class="column">1.4.4</div>
-						<div class="column">Minimum required version of the app</div>
-						<div class="column">10/05/2021 01:58</div>
-						<div class="column actions">
-							<button class="edit-btn">Edit</button>
-							<button class="delete-btn">Delete</button>
-						</div>
-					</div>
-					
-					<div class="row">
-						<div class="column">latest_version</div>
-						<div class="column">1.4.7</div>
-						<div class="column">Latest version of the app</div>
-						<div class="column">10/05/2021 01:58</div>
-						<div class="column actions">
-							<button class="edit-btn">Edit</button>
-							<button class="delete-btn">Delete</button>
-						</div>
-					</div>
-	
-					<div class="row">
-						<div class="column">pricing_tier</div>
-						<div class="column">t6</div>
-						<div class="column">Pricing tier of the user</div>
-						<div class="column">07/07/2021 11:13</div>
-						<div class="column actions">
-							<button class="edit-btn">Edit</button>
-							<button class="delete-btn">Delete</button>
-						</div>
-					</div>
-	
-					<div class="row">
-						<div class="column">scroll</div>
-						<div class="column">5</div>
-						<div class="column">Index of Scroll Payroll for free users</div>
-						<div class="column">25/08/2021 10:22</div>
-						<div class="column actions">
-							<button class="edit-btn">Edit</button>
-							<button class="delete-btn">Delete</button>
-						</div>
-					</div>
-	
-					<div class="row">
-						<div class="column">scroll_limit</div>
-						<div class="column">13</div>
-						<div class="column">Index of Scroll Limit Paywall for free users</div>
-						<div class="column">25/08/2021 10:22</div>
-						<div class="column actions">
-							<button class="edit-btn">Edit</button>
-							<button class="delete-btn">Delete</button>
-						</div>
-					</div>
-	
-				</div>
-	
-				<div class="add-row-container">
+			<div class="rows-container">
+				<div class="row" v-for="param in parameters" :key="param.id">
+					<div class="column">{{ param.key }}</div>
+					<div class="column">{{ param.value }}</div>
+					<div class="column">{{ param.description }}</div>
 					<div class="column">
-						<div class="input-container">
-							<input type="text" placeholder="New Parameter" />
-						</div>
-					</div>
-					<div class="column">
-						<div class="input-container">
-							<input type="text" placeholder="Value" />
-						</div>
-					</div>
-					<div class="column description-column">
-						<div class="input-container">
-							<input type="text" placeholder="New Description" />
-						</div>
+						{{ new Date(param.created.seconds * 1000).toLocaleDateString() }}
 					</div>
 					<div class="column actions">
-						<button class="add-btn">ADD</button>
+						<button class="edit-btn">Edit</button>
+						<button class="delete-btn">Delete</button>
 					</div>
 				</div>
 			</div>
 
+			<div class="add-row-container">
+				<div class="column">
+					<div class="input-container">
+						<input type="text" placeholder="New Parameter" />
+					</div>
+				</div>
+				<div class="column">
+					<div class="input-container">
+						<input type="text" placeholder="Value" />
+					</div>
+				</div>
+				<div class="column description-column">
+					<div class="input-container">
+						<input type="text" placeholder="New Description" />
+					</div>
+				</div>
+				<div class="column actions">
+					<button class="add-btn">ADD</button>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
 
 <script>
+import { getParameters } from "@/firestore"
+
 export default {
 	name: "Home",
+	data() {
+		return {
+			parameters: [],
+		}
+	},
+	async mounted() {
+		this.parameters = await getParameters()
+	},
 }
 </script>
 
 <style scoped>
-	.home {
-		width: 100vw;
-		height: 100vh;
-		margin: 0;
-		padding: 10px 2vw 10px 2vw;
-		display: flex;
-		justify-content: center;
-		flex-direction: column;
-	}
+.home {
+	width: 100vw;
+	height: 100vh;
+	margin: 0;
+	padding: 10px 2vw 10px 2vw;
+	display: flex;
+	justify-content: center;
+	flex-direction: column;
+}
 
-	.navbar {
-		display: flex;
-		justify-content: space-between;
-		width: 100%;
-		top: 0;
-		left: 0;
-		padding: 10px;
-	}
+.navbar {
+	display: flex;
+	justify-content: space-between;
+	width: 100%;
+	top: 0;
+	left: 0;
+	padding: 10px;
+}
 
-	.navbar img {
-		height: 30px;
-	}
+.navbar img {
+	height: 30px;
+}
 
-	.body-container {
-		display: flex;
-		flex: 1;
-		flex-direction: column;
-	}
+.body-container {
+	display: flex;
+	flex: 1;
+	flex-direction: column;
+	padding: 2rem;
+	color: #FFFFFF;
+}
+.header {
+	display: flex;
+	justify-content: space-between;
+	margin-bottom: 10px;
+}
 
-	.header-container {
-		display: flex;
-		flex: 1;
-		flex-direction: column;
-		padding: 2rem;
-		color: #FFFFFF;
-	}
-	
-	.header {
-		display: flex;
-		justify-content: space-between;
-		margin-bottom: 10px;
-	}
-	
-	.header .column {
-		color: rgb(124, 138, 164);
-		font-size: 28px;
-		font-family: 'Gilmer';
-		flex: 1;
-		text-align: left;
-		border-radius: 4px;
-	}
+.header .column {
+	color: rgb(124, 138, 164);
+	font-size: 28px;
+	font-family: "Gilmer";
+	flex: 1;
+	text-align: left;
+	border-radius: 4px;
+}
 
-	.rows-container {
-		display: flex;
-		flex-direction: column;
-	}
+.rows-container {
+	display: flex;
+	flex-direction: column;
+}
 
-	.row {
-		display: flex;
-		justify-content: space-between;
-		border-radius: 4px;
-	}
+.row {
+	display: flex;
+	justify-content: space-between;
+	border-radius: 4px;
+}
 
-	.row .column {
-		flex: 1;
-		text-align: left;
-		font-size: 16px;
-		font-family: 'Gilmer';
-	}
+.row .column {
+	flex: 1;
+	text-align: left;
+	font-size: 16px;
+	font-family: "Gilmer";
+}
 
-	.column .actions {
-		display: flex;
-		justify-content: space-between;
-	}
-	
-	.edit-btn, .delete-btn, .add-btn {
-		padding: 5px 10px;
-		margin: 5px;
-		border: none;
-		border-radius: 4px;
-		cursor: pointer;
-		color: white;
-		font-family: Verdana, Geneva, Tahoma, sans-serif;
-		font-weight: bold;
-	}
+.column .actions {
+	display: flex;
+	justify-content: space-between;
+}
 
-	.edit-btn {
-		background: linear-gradient(to bottom, rgb(64, 115, 236), rgb(60, 104, 236));
-	}
-	
-	.delete-btn {
-		background: linear-gradient(to bottom, rgb(225, 85, 96), rgb(220, 67, 50));
-	}
+.edit-btn,
+.delete-btn,
+.add-btn {
+	padding: 5px 10px;
+	margin: 5px;
+	border: none;
+	border-radius: 4px;
+	cursor: pointer;
+	color: white;
+	font-family: Verdana, Geneva, Tahoma, sans-serif;
+	font-weight: bold;
+}
 
-	.add-btn {
-		background: linear-gradient(to bottom, rgb(92, 200, 212), rgb(73, 162, 228));
-	}
+.edit-btn {
+	background: linear-gradient(to bottom, rgb(64, 115, 236), rgb(60, 104, 236));
+}
 
-	.add-row-container {
-		display: flex;
-		border-radius: 4px;
-	}
+.delete-btn {
+	background: linear-gradient(to bottom, rgb(225, 85, 96), rgb(220, 67, 50));
+}
 
-	.add-row-container .column {
-		flex: 1;
-	}
+.add-btn {
+	background: linear-gradient(to bottom, rgb(92, 200, 212), rgb(73, 162, 228));
+}
 
-	.add-row-container .description-column {
-		flex: 2;
-	}
-	
-	.input-container {
-		flex: 1;
-		padding: 0.5rem 2rem 0 0;
-	}
-	
-	.add-row-container input {
-		width: 100%;
-		padding: 0.5rem;
-		border: 1px solid #444;
-		border-radius: 4px;
-		background: none;
-		color: white;
-		font-family: 'Gilmer';
-	}
+.add-row-container {
+	display: flex;
+	border-radius: 4px;
+}
 
+.add-row-container .column {
+	flex: 1;
+}
+
+.add-row-container .description-column {
+	flex: 2;
+}
+
+.input-container {
+	flex: 1;
+	padding: 0.5rem 2rem 0 0;
+}
+
+.add-row-container input {
+	width: 100%;
+	padding: 0.5rem;
+	border: 1px solid #444;
+	border-radius: 4px;
+	background: none;
+	color: white;
+	font-family: "Gilmer";
+}
 </style>
