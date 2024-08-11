1775cf034c8ed3336ca9f972ac0a14b0064708d8
kindmemento <kindmemento@gmail.com>
2024-08-12 00:13:29 +0300

    import firebase config credentials from env variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,26 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDstTLfIXpNqoZJqStXmb7YPPPMRHrwDv0",
-  authDomain: "cortex-config.firebaseapp.com",
-  projectId: "cortex-config",
-  storageBucket: "cortex-config.appspot.com",
-  messagingSenderId: "370348194036",
-  appId: "1:370348194036:web:a438b70709fcd6467cc6a1",
-  measurementId: "G-DRSCNC93E1"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-export { auth };
+export { auth, db };
