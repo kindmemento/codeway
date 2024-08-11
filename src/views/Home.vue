272e26e38b5041c16dd83738e8fd2164446ce482
kindmemento <kindmemento@gmail.com>
2024-08-11 22:41:43 +0300

    simplify img el styling, add body container

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,82 +10,98 @@
 			</div>
 		</nav>
 
-		<div class="container">
-			<div class="header">
-				<div class="column">Parameter Key</div>
-				<div class="column">Value</div>
-				<div class="column">Description</div>
-				<div class="column">Create Date</div>
-				<div class="column"></div>
+		<div class="body-container">
+
+			<div class="header-container">
+				<div class="header">
+					<div class="column">Parameter Key</div>
+					<div class="column">Value</div>
+					<div class="column">Description</div>
+					<div class="column">Create Date</div>
+					<div class="column"></div>
+				</div>
+	
+				<div class="rows-container">
+	
+					<div class="row">
+						<div class="column">min_version</div>
+						<div class="column">1.4.4</div>
+						<div class="column">Minimum required version of the app</div>
+						<div class="column">10/05/2021 01:58</div>
+						<div class="column actions">
+							<button class="edit-btn">Edit</button>
+							<button class="delete-btn">Delete</button>
+						</div>
+					</div>
+					
+					<div class="row">
+						<div class="column">latest_version</div>
+						<div class="column">1.4.7</div>
+						<div class="column">Latest version of the app</div>
+						<div class="column">10/05/2021 01:58</div>
+						<div class="column actions">
+							<button class="edit-btn">Edit</button>
+							<button class="delete-btn">Delete</button>
+						</div>
+					</div>
+	
+					<div class="row">
+						<div class="column">pricing_tier</div>
+						<div class="column">t6</div>
+						<div class="column">Pricing tier of the user</div>
+						<div class="column">07/07/2021 11:13</div>
+						<div class="column actions">
+							<button class="edit-btn">Edit</button>
+							<button class="delete-btn">Delete</button>
+						</div>
+					</div>
+	
+					<div class="row">
+						<div class="column">scroll</div>
+						<div class="column">5</div>
+						<div class="column">Index of Scroll Payroll for free users</div>
+						<div class="column">25/08/2021 10:22</div>
+						<div class="column actions">
+							<button class="edit-btn">Edit</button>
+							<button class="delete-btn">Delete</button>
+						</div>
+					</div>
+	
+					<div class="row">
+						<div class="column">scroll_limit</div>
+						<div class="column">13</div>
+						<div class="column">Index of Scroll Limit Paywall for free users</div>
+						<div class="column">25/08/2021 10:22</div>
+						<div class="column actions">
+							<button class="edit-btn">Edit</button>
+							<button class="delete-btn">Delete</button>
+						</div>
+					</div>
+	
+				</div>
+	
+				<div class="add-row-container">
+					<div class="column">
+						<div class="input-container">
+							<input type="text" placeholder="New Parameter" />
+						</div>
+					</div>
+					<div class="column">
+						<div class="input-container">
+							<input type="text" placeholder="Value" />
+						</div>
+					</div>
+					<div class="column description-column">
+						<div class="input-container">
+							<input type="text" placeholder="New Description" />
+						</div>
+					</div>
+					<div class="column actions">
+						<button class="add-btn">ADD</button>
+					</div>
+				</div>
 			</div>
 
-			<div class="rows-container">
-
-				<div class="row">
-					<div class="column">min_version</div>
-					<div class="column">1.4.4</div>
-					<div class="column">Minimum required version of the app</div>
-					<div class="column">10/05/2021 01:58</div>
-					<div class="column actions">
-						<button class="edit-btn">Edit</button>
-						<button class="delete-btn">Delete</button>
-					</div>
-				</div>
-				
-				<div class="row">
-					<div class="column">latest_version</div>
-					<div class="column">1.4.7</div>
-					<div class="column">Latest version of the app</div>
-					<div class="column">10/05/2021 01:58</div>
-					<div class="column actions">
-						<button class="edit-btn">Edit</button>
-						<button class="delete-btn">Delete</button>
-					</div>
-				</div>
-
-				<div class="row">
-					<div class="column">pricing_tier</div>
-					<div class="column">t6</div>
-					<div class="column">Pricing tier of the user</div>
-					<div class="column">07/07/2021 11:13</div>
-					<div class="column actions">
-						<button class="edit-btn">Edit</button>
-						<button class="delete-btn">Delete</button>
-					</div>
-				</div>
-
-				<div class="row">
-					<div class="column">scroll</div>
-					<div class="column">5</div>
-					<div class="column">Index of Scroll Payroll for free users</div>
-					<div class="column">25/08/2021 10:22</div>
-					<div class="column actions">
-						<button class="edit-btn">Edit</button>
-						<button class="delete-btn">Delete</button>
-					</div>
-				</div>
-
-				<div class="row">
-					<div class="column">scroll_limit</div>
-					<div class="column">13</div>
-					<div class="column">Index of Scroll Limit Paywall for free users</div>
-					<div class="column">25/08/2021 10:22</div>
-					<div class="column actions">
-						<button class="edit-btn">Edit</button>
-						<button class="delete-btn">Delete</button>
-					</div>
-				</div>
-
-			</div>
-
-			<div class="add-row">
-				<div class="column"><input type="text" placeholder="New Parameter" /></div>
-				<div class="column"><input type="text" placeholder="Value" /></div>
-				<div class="column"><input type="text" placeholder="New Description" /></div>
-				<div class="column actions">
-					<button class="add-btn">ADD</button>
-				</div>
-			</div>
 		</div>
 	</div>
 </template>
@@ -114,39 +130,39 @@ export default {
 		top: 0;
 		left: 0;
 		padding: 10px;
-		/* border: 1px solid red; */
 	}
 
-	.navbar .logo img {
+	.navbar img {
 		height: 30px;
 	}
 
-	.navbar .profile img {
-		height: 30px;
+	.body-container {
+		display: flex;
+		flex: 1;
+		flex-direction: column;
 	}
 
-	.container {
+	.header-container {
 		display: flex;
 		flex: 1;
 		flex-direction: column;
 		padding: 2rem;
 		color: #FFFFFF;
 	}
-
+	
 	.header {
 		display: flex;
 		justify-content: space-between;
-		border-radius: 4px;
-		/* border: 1px solid red; */
+		margin-bottom: 10px;
 	}
-
+	
 	.header .column {
 		color: rgb(124, 138, 164);
 		font-size: 28px;
 		font-family: 'Gilmer';
 		flex: 1;
 		text-align: left;
-		border: 1px solid red;
+		border-radius: 4px;
 	}
 
 	.rows-container {
@@ -158,7 +174,6 @@ export default {
 		display: flex;
 		justify-content: space-between;
 		border-radius: 4px;
-		/* border: 1px solid red; */
 	}
 
 	.row .column {
@@ -166,7 +181,6 @@ export default {
 		text-align: left;
 		font-size: 16px;
 		font-family: 'Gilmer';
-		border: 1px solid red;
 	}
 
 	.column .actions {
@@ -197,26 +211,32 @@ export default {
 		background: linear-gradient(to bottom, rgb(92, 200, 212), rgb(73, 162, 228));
 	}
 
-	.add-row {
+	.add-row-container {
 		display: flex;
-		justify-content: space-between;
-		padding: 0.5rem;
 		border-radius: 4px;
-		margin-top: 0.5rem;
 	}
 
-	.add-row .column {
+	.add-row-container .column {
 		flex: 1;
-		padding: 0.5rem;
 	}
 
-	.add-row input {
+	.add-row-container .description-column {
+		flex: 2;
+	}
+	
+	.input-container {
+		flex: 1;
+		padding: 0.5rem 2rem 0 0;
+	}
+	
+	.add-row-container input {
 		width: 100%;
 		padding: 0.5rem;
 		border: 1px solid #444;
 		border-radius: 4px;
 		background: none;
 		color: white;
+		font-family: 'Gilmer';
 	}
 
 </style>
