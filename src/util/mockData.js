b4c50101b7d00db670a6978602ffe894dcae562a
kindmemento <kindmemento@gmail.com>
2024-08-13 01:51:05 +0300

    hardcode mock params data for ease of development

---
+++ src/util/mockData.js
@@ -0,0 +1,52 @@
+export const mockParams = [
+	{
+		id: 'OFRRJqSC5eVVdsOkXupY',
+		key: 'scroll_limit',
+		value: '13',
+		description: 'Index of Scroll Limit Paywall for free users.',
+		created: {
+			_seconds: 1629876180,
+			_nanoseconds: 500000000,
+		},
+	},
+	{
+		id: 'Z11ftICDmjmrzDVi6y3S',
+		key: 'scroll',
+		value: '5',
+		description: 'Index of Scroll Paywall for free users.',
+		created: {
+			_seconds: 1629876120,
+			_nanoseconds: 475000000,
+		},
+	},
+	{
+		id: 'ZhhaS9vOqAJgwUyqNFdb',
+		key: 'pricing_tier',
+		value: 't6',
+		description: 'Pricing tier of the user',
+		created: {
+			_seconds: 1625645580,
+			_nanoseconds: 37000000,
+		},
+	},
+	{
+		id: 'mMkJv9IN3RwLbNR6LvGC',
+		key: 'latest_version',
+		value: '1.4.7',
+		description: 'Latest version of the app',
+		created: {
+			_seconds: 1620601080,
+			_nanoseconds: 197000000,
+		},
+	},
+	{
+		id: 'xbPTsPluwBeFJKMqzsf6',
+		key: 'min_version',
+		value: '1.4.4',
+		description: 'Minimum required version of the app',
+		created: {
+			_seconds: 1620601080,
+			_nanoseconds: 800000000,
+		},
+	},
+]
