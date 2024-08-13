3b053dc48502da490edffebba7b74d1efc84f3b6
kindmemento <kindmemento@gmail.com>
2024-08-13 06:07:01 +0300

    implement auth store

---
+++ src/store/auth.js
@@ -0,0 +1,28 @@
+import firebase from '@/auth/firebase'
+
+export default {
+	namespaced: true,
+	state: {
+		user: null,
+	},
+	mutations: {
+		setUser(state, user) {
+			state.user = user
+		},
+	},
+	actions: {
+		async signIn({ commit }, { email, password }) {
+			const userCredential = await firebase.auth().signInWithEmailAndPassword(email, password)
+			commit('setUser', userCredential.user)
+		},
+		async signOut({ commit }) {
+			await firebase.auth().signOut()
+			commit('setUser', null)
+		},
+	},
+	getters: {
+		isAuthenticated(state) {
+			return !!state.user
+		},
+	},
+}
