0dc3bd996dd89cc53cefab953ac0aac7fc9dd1b7
kindmemento <kindmemento@gmail.com>
2024-08-14 13:13:31 +0300

    handle navbar

--- src/components/MobileHome.vue
+++ src/components/MobileHome.vue
@@ -1,8 +1,19 @@
 <template>
 	<div class="mobile-home">
-		<div class="navbar">
-			<!-- navbar content -->
-		</div>
+		<!-- Navbar -->
+		<nav class="navbar">
+			<div class="logo">
+				<img src="../assets//icon.png" alt="Codeway Logo" />
+			</div>
+
+			<div class="profile" @mouseenter="showDropdown" @mouseleave="hideDropdown">
+				<img src="../assets/profile-icon.png" alt="Profile Logo" />
+				<!-- @TODO: check issue below -->
+				<div class="dropdown-menu"> 
+					<button @click="signOut" class="dropdown-item">Sign Out</button>
+				</div>
+			</div>
+		</nav>
 		<div class="rows-container">
 			<div class="param-box" v-for="param in sortedParameters" :key="param.id">
 				<!-- param details for mobile layout -->
@@ -21,31 +32,33 @@ export default {
 	name: 'MobileHomePage',
 	setup() {
 		const {
+			sortOrder,
 			sortedParameters,
 			newParam,
 			createParameter,
 			enableEdit,
 			deleteParameter,
+			submitEdit,
 			editingParamId,
 			editedParam,
-			isDropdownVisible,
-			showDropdown,
-			hideDropdown,
 			toggleSort,
+			isDropdownVisible,
+			signOut,
 		} = useParameterLogic()
 
 		return {
+			sortOrder,
 			sortedParameters,
 			newParam,
 			createParameter,
 			enableEdit,
-			deleteParameter,
+			removeParameter: deleteParameter,
+			submitEdit,
 			editingParamId,
 			editedParam,
-			isDropdownVisible,
-			showDropdown,
-			hideDropdown,
 			toggleSort,
+			isDropdownVisible,
+			signOut,
 		}
 	},
 }
@@ -53,5 +66,37 @@ export default {
 
 <!-- @TODO: Handle CSS -->
 <style scoped>
+.mobile-home {
+	width: 100vw;
+	height: 100vh;
+	display: flex;
+	justify-content: center;
+	flex-direction: row;
+}
+
+.navbar {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	width: 100%;
+	height: min-content;
+	padding: 10px;
+	border: 1px solid red;
+}
+
+.logo img {
+	height: 30px;
+}
+
+.profile {
+	position: relative;
+	display: flex;
+	/* padding: 0 25px; */
+	cursor: pointer;
+}
+
+.profile img {
+	height: 45px;
+}
 
 </style>
