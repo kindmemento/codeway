86634be8da98e2b21b4ef62009788ff0a731e9e9
kindmemento <kindmemento@gmail.com>
2024-08-14 11:37:26 +0300

    implement boilerplate layout for mobile home view

---
+++ src/components/MobileHome.vue
@@ -0,0 +1,57 @@
+<template>
+	<div class="mobile-home">
+		<div class="navbar">
+			<!-- navbar content -->
+		</div>
+		<div class="rows-container">
+			<div class="param-box" v-for="param in sortedParameters" :key="param.id">
+				<!-- param details for mobile layout -->
+			</div>
+			<div class="param-box add-new">
+				<!-- add new param form for mobile layout -->
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import { useParameterLogic } from '@/composables/useParameterLogic'
+
+export default {
+	name: 'MobileHomePage',
+	setup() {
+		const {
+			sortedParameters,
+			newParam,
+			createParameter,
+			enableEdit,
+			deleteParameter,
+			editingParamId,
+			editedParam,
+			isDropdownVisible,
+			showDropdown,
+			hideDropdown,
+			toggleSort,
+		} = useParameterLogic()
+
+		return {
+			sortedParameters,
+			newParam,
+			createParameter,
+			enableEdit,
+			deleteParameter,
+			editingParamId,
+			editedParam,
+			isDropdownVisible,
+			showDropdown,
+			hideDropdown,
+			toggleSort,
+		}
+	},
+}
+</script>
+
+<!-- @TODO: Handle CSS -->
+<style scoped>
+
+</style>
